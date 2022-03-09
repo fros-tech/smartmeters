19fa51b4c3daa7b1782bf2210586eb9132acb625
Frank <fros@techcollege.dk>
2022-03-09 14:56:43 +0100

    Initial commit

---
+++ SmartMeter/Consumer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Consumer.Implementations;
+using System;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        private const string topic = "someme-logs";
+        public static async Task Main(string[] args)
+        {
+            Console.Title = "Kafka Sample Consumer";
+            Console.WriteLine("Kafka Sample Consumer");
+            SampleConsumer consumer = new SampleConsumer();
+            await consumer.SubscribeAsync(topic, Console.WriteLine);
+        }
+    }
+}
